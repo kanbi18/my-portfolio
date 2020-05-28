f3a52ca0d62ccd61cb5ff60fb43139cadc2aaa53
kanbi18 <ade.akinola1846@gmail.com>
2020-05-28 10:05:06 -0500

    My first portofolio commit

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,3 +1,5 @@
+
+
 // Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,18 +13,36 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 /**
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
   const greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
+ 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
+ 
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+ 
+function sayRandomQuote(){
+    const quotes = [
+        "Elementary, my dear Watson",
+        "You miss 100% of the shots you don't take." ,
+        "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        "The future belongs to those who believe in the beauty of their dreams.",
+        "Do not go where the path may lead, go instead where there is no path and leave a trail." ,
+        "Life is really simple, but we insist on making it complicated." ,
+    ];
+ 
+    const chosenQuote= quotes[Math.floor(Math.random() * quotes.length)];
+ 
+    const quoteContainer = document.getElementById("quote-container");
+    quoteContainer.innerHTML = chosenQuote;
+ 
+}
+ 
